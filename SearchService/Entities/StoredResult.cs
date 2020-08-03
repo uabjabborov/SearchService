815a58471f69d329b4d603fbc2a7f3139b3aa589
ulfat <ulfat@shazzle.com>
2020-08-03 13:38:11 +0500

    add Entity and Storage namespaces

---
+++ SearchService/Entities/StoredResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SearchService.Entities
+{
+    public class StoredResult
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ SearchService/Storages/DbStorage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SearchService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace SearchService.Storages
+{
+    public class DbStorage<T> : StorageInterface where T : DbContext, System.IDisposable, new()
+    {
+        public Task<List<SearchResult>> searchAsync(string keyword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> storeAsync(List<SearchResult> results)
+        {
+            try
+            {
+                using (T db = new T())
+                {
+                    db.Add(new StoredResult());
+                    var res = await db.SaveChangesAsync();
+
+                    return res > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
+        }
+    }
+}
