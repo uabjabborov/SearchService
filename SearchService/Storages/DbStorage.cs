f9b4464f727e68aa10c2deb1065d5c4c3da3c702
ulfat <ulfat@shazzle.com>
2020-08-03 15:52:08 +0500

    add DbStorage.searchAsync impl

--- SearchService/Storages/DbStorage.cs
+++ SearchService/Storages/DbStorage.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using SearchService.Entities;
+using SearchService.Databases;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace SearchService.Storages
 {
-    public class DbStorage<T> : StorageInterface where T : DbContext, System.IDisposable, new()
+    public class DbStorage<T> : StorageInterface where T : SqliteDB, System.IDisposable, new()
     {
-        public Task<List<SearchResult>> searchAsync(string keyword)
+        public async Task<List<SearchResult>> searchAsync(string keyword)
         {
-            throw new NotImplementedException();
+            using(T db = new T())
+            {
+                var results = await db.StoredResults
+                                .Where(r => r.Link.ToLower().Contains(keyword.ToLower()) ||
+                                            r.Text.ToLower().Contains(keyword.ToLower()) ||
+                                            r.Title.ToLower().Contains(keyword.ToLower()))
+                                .ToListAsync();
+
+                return results.ConvertAll(x => new SearchResult { Link = x.Link, Text = x.Text, Title = x.Title });
+            }
         }
 
         public async Task storeAsync(List<SearchResult> results)
