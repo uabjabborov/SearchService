bcd0b3e447bec84a759148d5079869e2e989fccb
ulfat <ulfat@shazzle.com>
2020-08-04 20:25:10 +0500

    add search controllers

---
+++ SearchServiceWeb/Controllers/OfflineSearchController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SearchService;
+
+namespace SearchServiceWeb.Controllers
+{
+    public class OfflineSearchController : Controller
+    {
+        private static Service GetService()
+        {
+            List<SearchEngineInterface> searchEngines = new List<SearchEngineInterface>();
+            var storage = new SearchService.Storages.DbStorage<SearchService.Databases.SqliteDB>();
+
+            return new Service(storage, searchEngines);
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index(string searchString)
+        {
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                searchString = searchString.Trim();
+                ViewData["CurrentFilter"] = searchString;
+
+                return View(await GetService().SearchOfflineAsync(searchString));
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SearchServiceWeb/Controllers/OnlineSearchController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SearchService;
+using SearchServiceTest;
+
+namespace SearchServiceWeb.Controllers
+{
+    public class OnlineSearchController : Controller
+    {
+        private static Service GetService()
+        {
+            List<SearchEngineInterface> searchEngines = new List<SearchEngineInterface>();
+
+            searchEngines.Add(new SearchService.SearchEngines.Google());
+            searchEngines.Add(new SearchService.SearchEngines.Yandex());
+            searchEngines.Add(new SearchService.SearchEngines.Bing());
+
+            var storage = new SearchService.Storages.DbStorage<SearchService.Databases.SqliteDB>();
+
+            return new Service(storage, searchEngines);
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index(string searchString)
+        {
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                searchString = searchString.Trim();
+                ViewData["CurrentFilter"] = searchString;
+
+                return View(await GetService().SearchOnlineAsync(searchString));
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
