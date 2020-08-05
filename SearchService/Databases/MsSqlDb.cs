d11bc671c1dcec64a3f025ffe78dc13cd0321473
ulfat <ulfat@shazzle.com>
2020-08-05 10:42:27 +0500

    add MsSqlDb.cs

---
+++ SearchService/Databases/MsSqlDb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SearchService.Entities;
+
+namespace SearchService.Databases
+{
+    public class MsSqlDb : CommonDb
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("");
+    }
+}
